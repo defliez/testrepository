74135b2de918d9da0ab014ab9577627f0496f5af
Taisiralani <Taisiralani@gmail.com>
2022-04-08 12:44:58 +0200

    test

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,15 +7,18 @@ public class PlayerController : MonoBehaviour {
     public float speed = 5;
     public Rigidbody rb;
 
+    float horizontalInput;
+
     private void FixedUpdate ()
     {
         Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
-        rb.MovePosition(rb.position + forwardMove);
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
     }
 
     // Update is called once per frame
     private void Update () {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
     }
 
 }
