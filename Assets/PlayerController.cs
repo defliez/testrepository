85829f8bfbf8185cacb76185d17d578129020f1f
Valentino Glave <defliez@protonmail.com>
2022-04-16 01:00:53 +0200

    Testing new methods and instance variables in in PlayerController script

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+<<<<<<< Updated upstream:Assets/PlayerController.cs
 public class PlayerController : MonoBehaviour
+=======
+//[RequireComponent(typeof(Rigidbody))]
+public class SC_PlayerController : MonoBehaviour
+>>>>>>> Stashed changes:Assets/Scripts/SC_PlayerController.cs
 {
+    public float gravity = 20.0f;
     public Rigidbody rb;
+    bool grounded = false;
+    Vector3 defaultScale;
     public float moveSpeed = 10f;
 
     private float xInput;
@@ -12,9 +20,13 @@ public class PlayerController : MonoBehaviour
 
 
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+        rb.freezeRotation = true;
+        rb.useGravity = false;
+        defaultScale = transform.localScale;
     }
 
     // Update is called once per frame
@@ -26,6 +38,8 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
+
+        grounded = false;
     }
 
     private void ProcessInputs()
@@ -36,6 +50,22 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
-        rb.AddForce(new Vector3(xInput, 0f, zInput) * moveSpeed);
+        //rb.AddForce(new Vector3(xInput, 0f, zInput) * moveSpeed);
+        rb.AddForce(new Vector3(0, -gravity * rb.mass, 0));
     }
+
+    void OnCollisionStay()
+    {
+        grounded = true;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Finish")
+        {
+            //print("GameOver!");
+            SC_GroundGenerator.instance.gameOver = true;
+        }
+    }
+
+
 }
