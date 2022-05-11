a0cf7fb5d9a982838a400977400b823c7569bfae
Albin Halit <Albinhaliti16@hotmail.com>
2022-05-11 16:12:25 +0200

    test

--- Assets/Scenes/PlayerMovement.cs
+++ Assets/Scenes/PlayerMovement.cs
@@ -5,12 +5,24 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody rb;
 
+    public float forwardForce = 2000f;
+     public float sidewaysForce = 500f;
+
     // Start is called before the first frame update
 
     // Update is called once per frame
     void FixedUpdate()
     {
         rb.AddForce(0, 0, 2000 * Time.deltaTime);
+
+        if(Input.GetKey("d")){
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        }
+        
+         if(Input.GetKey("a")){
+            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+        }
         
     }
 }
+
