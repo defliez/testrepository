83ec7cade4e844dc9fa55b1289349615ef744d8a
Albin Halit <Albinhaliti16@hotmail.com>
2022-05-11 15:38:45 +0200

    test

--- Assets/Scenes/PlayerMovement.cs
+++ Assets/Scenes/PlayerMovement.cs
@@ -2,14 +2,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public Rigidbody rb;
+
     // Start is called before the first frame update
-    void Start(){
-        Debug.Log("PlayerMovement");
-    }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
         
     }
 }
