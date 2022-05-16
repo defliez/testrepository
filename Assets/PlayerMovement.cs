8972238b2bb5f0b7df2f3338f504f1f36169fed5
Albin Halit <Albinhaliti16@hotmail.com>
2022-05-16 18:30:16 +0200

    fix

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     
+    public float speed = 5;
+    public Rigidbody rb;
+
+    private void FixedUpdate() {
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + forwardMove);
+    }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
     }
 }
