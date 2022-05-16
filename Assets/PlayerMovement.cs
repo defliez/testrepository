77c8f41acb6af2e653de12f282db38e66241b7ac
Albin Halit <Albinhaliti16@hotmail.com>
2022-05-16 18:46:48 +0200

    fix

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,8 +7,12 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5;
     public Rigidbody rb;
 
+    float horizontalInput;
+    
+
     private void FixedUpdate() {
         Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime;
         rb.MovePosition(rb.position + forwardMove);
     }
 
