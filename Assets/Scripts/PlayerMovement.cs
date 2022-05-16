8f6d638289b7772966681d9ae05a54187f6c2ddb
Albin Halit <Albinhaliti16@hotmail.com>
2022-05-16 18:17:28 +0200

    Fix

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+   
+    public float speed = 5;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
